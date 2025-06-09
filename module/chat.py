9e672765590facc7b8053c64973a028ee2fafc82
KrashMello <joelenrikesoteldo@gmail.com>
2025-06-09 18:32:39 -0400

    feat: update config data and messages

--- module/chat.py
+++ module/chat.py
@@ -5,7 +5,13 @@ import json
 import threading
 import sys
 from time import sleep
-from module.config import get_ia_config
+from module.config import (
+    get_ia_config,
+    set_config_provider,
+    set_config_provider_api_key,
+    set_config_provider_model,
+    set_config_provider_url,
+)
 from rich.console import Console
 from rich.markdown import Markdown
 
@@ -23,13 +29,20 @@ def reset_chat():
     messages = []  # Limpiamos el messages
 
 
-def ask():
+def ask(question):
     global api_key, provider, url, model
     global messages
+
     if not api_key:
-        print("\nError: No se encontró la API key de Groq")
-        print("Por favor, configura la variable de entorno GROQ_API_KEY")
-        print("En Windows puedes usar: set GROQ_API_KEY=tu-api-key")
+        print("Por favor, configura la API_KEY con el comando /setApiKey")
+        return ""
+    if not question:
+        print(
+            "no se pudo generar la respuesta debido a que no se ha ingresado una pregunta"
+        )
+        return ""
+    if not provider:
+        print("Por favor, configura el proveedor con el comando /setProvider")
         return ""
 
     URL = ""
@@ -37,30 +50,33 @@ def ask():
         "Content-Type": "application/json",
     }
     body = None
-    match provider:
-        case "groq":
-            URL = url if url is not None else ""
-            headers["Authorization"] = f"Bearer {api_key}"
-            body = {
-                "model": model,
-                "messages": messages,
+
+    if provider == "groq":
+        URL = url if url is not None else ""
+        headers["Authorization"] = f"Bearer {api_key}"
+        body = {
+            "model": model,
+            "messages": messages,
+        }
+        messages.append(
+            {
+                "role": "user",
+                "content": question,
             }
-        case "gemini":
-            URL = (
-                f"{url}/{model}:generateContent?key={api_key}"
-                if url is not None
-                else ""
-            )
-            body = {
-                "contents": messages,
+        )
+    if provider == "gemini":
+        URL = f"{url}/{model}:generateContent?key={api_key}" if url is not None else ""
+        body = {
+            "contents": messages,
+        }
+        messages.append(
+            {
+                "role": "user",
+                "parts": {
+                    "text": question,
+                },
             }
-        case _:
-            URL = url if url is not None else ""
-            headers["Authorization"] = f"Bearer {api_key}"
-            body = {
-                "model": model,
-                "messages": messages,
-            }
+        )
 
     try:
         response = requests.post(URL, headers=headers, data=json.dumps(body))
@@ -94,45 +110,71 @@ def help():
     print("/new: Reinicia el chat")
     print("/exit: Salir del chat")
     print("/help: Mostrar este mensaje")
+    print("/setApiKey: Guardar la API key")
+    print("/setProvider: Cambiar el proveedor")
+    print("/setUrl: Cambiar la URL del proveedor")
+    print("/setModel: Cambiar el modelo del proveedor")
+    print("clear: limpiar la pantalla pero mantiene la conversacion")
+
+
+def clear():
+    global provider, model
+    os.system("cls" if os.name == "nt" else "clear")
+    print("Chat AI - Tui")
+    print("Ingrese tus preguntas y recibirás respuestas")
+    print("Usa el comando /help para ver los comandos disponibles")
+    print(f"Usando el proveedor {provider}")
+    print(f"Usando el modelo {model}")
 
 
 def chat():
-    global exit, provider
+    global exit, provider, model, api_key, url
+    clear()
     while not exit:
         question = input("> ")
         match question:
+            case "clear":
+                clear()
             case "/new":
                 reset_chat()
-                os.system("cls" if os.name == "nt" else "clear")
+                clear()
             case "/exit":
                 exit = True
             case "/help":
                 help()
+            case "/setApiKey":
+                api_key = input("Ingrese la API key: ")
+                set_config_provider_api_key(
+                    provider if provider is not None else "groq", api_key
+                )
+                api_key = get_ia_config("API_KEY")
+                print("API key guardada")
+            case "/setProvider":
+                sprovider = input("Ingrese el proveedor: ")
+                seter = set_config_provider(sprovider)
+                if seter is not None:
+                    provider = get_ia_config("PROVIDER")
+                    api_key = get_ia_config("API_KEY")
+                    url = get_ia_config("URL")
+                    model = get_ia_config("MODEL")
+                    reset_chat()
+                    clear()
+                    print("Proveedor guardado")
+            case "/setUrl":
+                url = input("Ingrese la URL del proveedor: ")
+                set_config_provider_url(
+                    provider if provider is not None else "groq", url
+                )
+                url = get_ia_config("URL")
+                print("URL guardada")
+            case "/setModel":
+                model = input("Ingrese el modelo del proveedor: ")
+                set_config_provider_model(
+                    provider if provider is not None else "groq", model
+                )
+                model = get_ia_config("MODEL")
+                print("Modelo guardado")
             case _:
-                match provider:
-                    case "groq":
-                        messages.append(
-                            {
-                                "role": "user",
-                                "content": question,
-                            }
-                        )
-                    case "gemini":
-                        messages.append(
-                            {
-                                "role": "user",
-                                "parts": {
-                                    "text": question,
-                                },
-                            }
-                        )
-                    case _:
-                        messages.append(
-                            {
-                                "role": "user",
-                                "content": question,
-                            }
-                        )
-                message = ask()
+                message = ask(question)
                 if message:
                     console.print(Markdown(message))
--- module/config.py
+++ module/config.py
@@ -4,17 +4,100 @@ from dotenv import load_dotenv
 
 load_dotenv()
 HOME_DIR = os.path.expanduser("~")
+providers = ["groq", "gemini"]
+default_config = """
+[global]
+provider="gemini"
+theme="nord"
+[groq]
+URL="https://api.groq.com/openai/v1/chat/completions"
+MODEL="llama-3.3-70b-versatile"
+API_KEY="api_key"
+[gemini]
+URL="https://generativelanguage.googleapis.com/v1beta/models"
+MODEL="gemini-2.0-flash"
+API_KEY="api_key"
+"""
+path_config = HOME_DIR + "/.config/chat-ai/config.toml"
 
 
 def get_ia_config(type: str):
+    if os.path.exists(path_config):
+        try:
+            with open(path_config, "r") as archivo:
+                configuracion = toml.load(archivo)
+            provider = configuracion["global"]["provider"]
+
+            if type == "PROVIDER":
+                return provider
+            else:
+                return configuracion[provider][type]
+        except Exception as e:
+            print("Error al cargar la configuración:", str(e))
+    else:
+        try:
+            os.makedirs(os.path.dirname(path_config), exist_ok=True)
+            with open(path_config, "w") as archivo:
+                toml.dump(toml.loads(default_config), archivo)
+
+            with open(path_config, "r") as archivo:
+                configuracion = toml.load(archivo)
+
+            provider = configuracion["global"]["provider"]
+
+            if type == "PROVIDER":
+                return provider
+            else:
+                return configuracion[provider][type]
+        except Exception as e:
+            print("Error al cargar la configuración:", str(e))
+
+
+def set_config_provider_api_key(provider: str, api_key: str):
     try:
         with open(HOME_DIR + "/.config/chat-ai/config.toml", "r") as archivo:
             configuracion = toml.load(archivo)
-        provider = configuracion["global"]["provider"]
+        configuracion[provider]["API_KEY"] = api_key
+        with open(HOME_DIR + "/.config/chat-ai/config.toml", "w") as archivo:
+            toml.dump(configuracion, archivo)
+    except Exception as e:
+        print("Error al cargar la configuración:", str(e))
 
-        if type == "PROVIDER":
-            return provider
+
+def set_config_provider(provider: str):
+    global providers
+    try:
+        if provider not in providers:
+            print(f"los proveedores disponibles son: {', '.join(providers)}")
+            return None
         else:
-            return configuracion[provider][type]
+            with open(HOME_DIR + "/.config/chat-ai/config.toml", "r") as archivo:
+                configuracion = toml.load(archivo)
+            configuracion["global"]["provider"] = provider
+            with open(HOME_DIR + "/.config/chat-ai/config.toml", "w") as archivo:
+                toml.dump(configuracion, archivo)
+            return True
+    except Exception as e:
+        print("Error al cargar la configuración:", str(e))
+
+
+def set_config_provider_url(provider: str, url: str):
+    try:
+        with open(HOME_DIR + "/.config/chat-ai/config.toml", "r") as archivo:
+            configuracion = toml.load(archivo)
+        configuracion[provider]["URL"] = url
+        with open(HOME_DIR + "/.config/chat-ai/config.toml", "w") as archivo:
+            toml.dump(configuracion, archivo)
+    except Exception as e:
+        print("Error al cargar la configuración:", str(e))
+
+
+def set_config_provider_model(provider: str, model: str):
+    try:
+        with open(HOME_DIR + "/.config/chat-ai/config.toml", "r") as archivo:
+            configuracion = toml.load(archivo)
+        configuracion[provider]["MODEL"] = model
+        with open(HOME_DIR + "/.config/chat-ai/config.toml", "w") as archivo:
+            toml.dump(configuracion, archivo)
     except Exception as e:
         print("Error al cargar la configuración:", str(e))
